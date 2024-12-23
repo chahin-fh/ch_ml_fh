536127c5d29645dc5b352368cb91627c74ccd188
Fhima C. <chahinfhima@gmail.com>
2024-12-23 18:12:21 +0100

    up3

--- connexion/main1.php
+++ connexion/main1.php
@@ -5,9 +5,10 @@ $cnx = mysqli_connect("127.0.0.1","root","","ifbreak");
 $req = "SELECT nom,passe,id from personne where(nom = '$nom' and passe = '$mp');" or die("problim L4");
 $res = mysqli_query($cnx,$req);
 $nb=mysqli_num_rows($res);
-
 if($nb<1){
-    die("makich mawjoud m3ana dsl , jarab mara o5ra");
+
+    header("refresh:0;url=index.html"); 
+    echo '<script>alert("makich m3ana a 5ouya !");</script>';
 }else{
     $id = mysqli_fetch_array($res);
     $_SESSION['id']=$id[2];
